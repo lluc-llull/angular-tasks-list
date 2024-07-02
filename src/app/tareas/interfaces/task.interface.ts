02a0ff2c4ab8027b42e2859caabf3645b75646dd
Lluc Llull <lluc@clicktotravel.es>
2024-07-02 14:09:04 +0200

    change interface

--- src/app/tareas/interfaces/task.interface.ts
+++ src/app/tareas/interfaces/task.interface.ts
@@ -2,4 +2,18 @@ export interface Task {
   id: number;
   title: string;
   completed: boolean;
+  priority: Priority;
+  createdAt: Date;
+  dueDate: Date;
+  description: string;
+}
+
+export interface Priority {
+  id: number;
+  name: string;
+}
+
+export interface Status {
+  id: number;
+  name: string;
 }
